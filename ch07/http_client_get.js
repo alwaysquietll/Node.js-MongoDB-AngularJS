da51041623734586f8b603be90d8f97494730f52
jack <alwaysquietll@gmail.com>
2020-01-10 17:45:24 +0800

    ch07 http_client_get.js

---
+++ ch07/http_client_get.js
@@ -0,0 +1,22 @@
+/**
+ * Created by jack on 2020/1/10.
+ */
+var http = require('http');
+var options = {
+    hostname: 'localhost',
+    port: '8080',
+};
+function handleResponse(response){
+    var serverData = '';
+    response.on('data', function(chunk){
+        serverData += chunk;
+    });
+    response.on('end', function(){
+        console.log("Response Status:", response.statusCode);
+        console.log("Response Headers:", response.headers);
+        console.log(serverData);
+    });
+}
+http.request(options, function(response){
+    handleResponse(response);
+}).end();
