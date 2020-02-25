5d4b1511060d7a49606826a434d2a9a672ac86c4
alwaysquietll <alwaysquietll@gmail.com>
2020-02-25 16:12:18 +0800

    ch16 word_schema.js

--- ch16/word_schema.js
+++ ch16/word_schema.js
@@ -1,15 +1,31 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var wordSchema = new Schema({
-    word: {type: String, index: 1, required:true, unique: true},
+    word: {type: String, index: 1, required: true},
     first: {type: String, index: 1},
     last: String,
     size: Number,
     letters: [String],
     stats: {
-        vowels:Number, consonants:Number},
-    charsets: [{ type: String, chars: [String]}]
-}, {collection: 'word_stats'});
+        vowels: Number,
+        consonants: Number
+    },
+    charsets: [{
+        type: String,
+        chars: [String]
+    }]
+},{collection: 'word_stats'});
+//past code,problem is "unique: true"
+// var wordSchema = new Schema({
+//     word: {type: String, index: 1, required:true, unique: true},
+//     first: {type: String, index: 1},
+//     last: String,
+//     size: Number,
+//     letters: [String],
+//     stats: {
+//         vowels:Number, consonants:Number},
+//     charsets: [{ type: String, chars: [String]}]
+// }, {collection: 'word_stats'});
 wordSchema.methods.startsWith = function(letter){
     return this.first === letter;
 };
@@ -18,3 +34,10 @@ console.log("Required Paths: ");
 console.log(wordSchema.requiredPaths());
 console.log("Indexes: ");
 console.log(wordSchema.indexes());
+// var s = new Schema({
+//     name: { type: String, required: true },
+//     age: { type: String, required: true },
+//     notes: String
+// });
+// s.requiredPaths(); // [ 'age', 'name' ]
+// console.log(s.requiredPaths());
